4b0014eadc0c891fe65bb8f3e32f91643e05fbb8
Liya Tian <t629715@163.com>
2019-05-10 11:04:44 +0800

    初始化项目

---
+++ src/main/java/com/example/listenerfilechange/ListenerFileChangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.listenerfilechange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenerFileChangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListenerFileChangeApplication.class, args);
+    }
+
+}
